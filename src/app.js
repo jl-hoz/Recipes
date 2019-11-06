c48905902b88db3a4dae1004f0f920c95b9d61b9
José Luis de la Hoz <joseluisdelahozgarcia@gmail.com>
2019-11-06 20:31:48 +0100

    Delete Recipe

--- src/app.js
+++ src/app.js
@@ -68,9 +68,9 @@ const typeDefs = `
         addRecipe(title: String!, description: String!, author: ID!, ingredients: [ID!]): Recipe!
         addAuthor(name: String!, email: String!): Author!
         addIngredient(name: String!): Ingredient!
-        deleteRecipe(id: ID!): Recipe!
-        deleteAuthor(id: ID!): Author!
-        deleteIngredient(id: ID!): Ingredient!
+        deleteRecipe(id: ID!): Boolean!
+        deleteAuthor(id: ID!): Boolean!
+        deleteIngredient(id: ID!): Boolean!
         updateRecipe(id: ID!, title: String, description: String, author: ID, ingredients: [ID]): Recipe!
         updateAuthor(id: ID!, name: String, email: String, recipes: [ID]): Author!
         updateIngredient(id: ID!, name: String, recipes: [ID]): Ingredient!
@@ -200,8 +200,25 @@ const resolvers = {
             return ingredient;
         },
 	deleteRecipe: (parent, args, ctx, info) => {
-
-	}
+	    let flag = false;
+	    const recipe = recipesData.find(recipe => recipe.id === args.id);
+	    if(recipe){
+		flag = true;
+		const authorID = recipe.author;
+		const ingredients = recipe.ingredients;
+		//Delete reference of recipe in author
+		const author = authorsData.find(author => author.id === authorID);
+		author.recipes.splice(author.recipes.indexOf(args.id), 1);
+		//Delete each reference of recipe in ingredients array
+		ingredients.map(ingredientID => {
+		    const ingredient = ingredientsData.find(ingredient => ingredient.id === ingredientID);
+		    ingredient.recipes.splice(ingredient.recipes.indexOf(args.id), 1);
+		});
+		//Delete Recipe
+		recipesData.splice(recipesData.indexOf(recipe), 1);
+	    }
+	    return flag;
+	},
     },
 
 };
